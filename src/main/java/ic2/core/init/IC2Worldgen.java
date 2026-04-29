629ad14baadb6f9f98b6d8493c2c1eb529baf4a2
Llemneed <igrokk626@gmail.com>
2026-04-29 21:16:09 +0500

    Add CI and align worldgen with IC2.ini

--- src/main/java/ic2/core/init/IC2Worldgen.java
+++ src/main/java/ic2/core/init/IC2Worldgen.java
@@ -8,11 +8,13 @@ import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
 public final class IC2Worldgen {
+    public static final ResourceKey<ConfiguredFeature<?, ?>> COPPER_ORE = configured("copper_ore");
     public static final ResourceKey<ConfiguredFeature<?, ?>> LEAD_ORE = configured("lead_ore");
     public static final ResourceKey<ConfiguredFeature<?, ?>> TIN_ORE = configured("tin_ore");
     public static final ResourceKey<ConfiguredFeature<?, ?>> URANIUM_ORE = configured("uranium_ore");
     public static final ResourceKey<ConfiguredFeature<?, ?>> RUBBER_TREE = configured("rubber_tree");
 
+    public static final ResourceKey<PlacedFeature> COPPER_ORE_PLACED = placed("copper_ore");
     public static final ResourceKey<PlacedFeature> LEAD_ORE_PLACED = placed("lead_ore");
     public static final ResourceKey<PlacedFeature> TIN_ORE_PLACED = placed("tin_ore");
     public static final ResourceKey<PlacedFeature> URANIUM_ORE_PLACED = placed("uranium_ore");
