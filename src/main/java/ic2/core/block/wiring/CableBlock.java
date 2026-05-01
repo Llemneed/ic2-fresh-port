796d8809dc083b1d767d62004413d74449a44ddb
Llemneed <igrokk626@gmail.com>
2026-05-01 17:05:38 +0500

    Add state-driven cable connection models

--- src/main/java/ic2/core/block/wiring/CableBlock.java
+++ src/main/java/ic2/core/block/wiring/CableBlock.java
@@ -3,15 +3,29 @@ package ic2.core.block.wiring;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.core.Direction;
+import ic2.core.energy.EnergyConsumer;
+import ic2.core.energy.EnergyTransport;
 
 public final class CableBlock extends BaseEntityBlock implements EntityBlock {
+    public static final BooleanProperty NORTH = BooleanProperty.create("north");
+    public static final BooleanProperty EAST = BooleanProperty.create("east");
+    public static final BooleanProperty SOUTH = BooleanProperty.create("south");
+    public static final BooleanProperty WEST = BooleanProperty.create("west");
+    public static final BooleanProperty UP = BooleanProperty.create("up");
+    public static final BooleanProperty DOWN = BooleanProperty.create("down");
     private final CableType cableType;
     private final MapCodec<CableBlock> codec;
 
@@ -19,6 +33,13 @@ public final class CableBlock extends BaseEntityBlock implements EntityBlock {
         super(properties);
         this.cableType = cableType;
         this.codec = MapCodec.unit(this);
+        registerDefaultState(defaultBlockState()
+                .setValue(NORTH, false)
+                .setValue(EAST, false)
+                .setValue(SOUTH, false)
+                .setValue(WEST, false)
+                .setValue(UP, false)
+                .setValue(DOWN, false));
     }
 
     public CableType getCableType() {
@@ -41,7 +62,45 @@ public final class CableBlock extends BaseEntityBlock implements EntityBlock {
     }
 
     @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return updateConnections(defaultBlockState(), context.getLevel(), context.getClickedPos());
+    }
+
+    @Override
+    protected BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos pos, BlockPos neighborPos) {
+        return state.setValue(propertyFor(direction), canConnect(level, neighborPos));
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(NORTH, EAST, SOUTH, WEST, UP, DOWN);
+    }
+
+    @Override
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(net.minecraft.world.level.Level level, BlockState state, BlockEntityType<T> blockEntityType) {
         return null;
+    }
+
+    private static BlockState updateConnections(BlockState state, LevelAccessor level, BlockPos pos) {
+        for (Direction direction : Direction.values()) {
+            state = state.setValue(propertyFor(direction), canConnect(level, pos.relative(direction)));
+        }
+        return state;
+    }
+
+    private static boolean canConnect(LevelAccessor level, BlockPos pos) {
+        BlockEntity blockEntity = level.getBlockEntity(pos);
+        return blockEntity instanceof EnergyTransport || blockEntity instanceof EnergyConsumer;
+    }
+
+    private static BooleanProperty propertyFor(Direction direction) {
+        return switch (direction) {
+            case NORTH -> NORTH;
+            case EAST -> EAST;
+            case SOUTH -> SOUTH;
+            case WEST -> WEST;
+            case UP -> UP;
+            case DOWN -> DOWN;
+        };
     }
 }
